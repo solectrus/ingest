fb5f0386df81b7c5d19c5d85c81104ee567e996a
Georg Ledermann <georg@ledermann.dev>
2025-03-23 11:37:47 +0100

    Update store_spec.rb

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,7 +2,12 @@ describe Store do
   let(:store) { described_class.new(DB_TEST) }
 
   it 'saves integer values correctly' do
-    store.save(measurement: 'SENEC', field: 'power', timestamp: 1000, value: 42)
+    store.save(
+      measurement: 'SENEC',
+      field: 'inverter_power',
+      timestamp: 1000,
+      value: 42,
+    )
     row = DB_TEST[:sensor_data].first
     expect(row[:value_int]).to eq(42)
   end
@@ -10,7 +15,7 @@ describe Store do
   it 'saves float values correctly' do
     store.save(
       measurement: 'SENEC',
-      field: 'power',
+      field: 'inverter_power',
       timestamp: 1000,
       value: 42.5,
     )
@@ -21,47 +26,57 @@ describe Store do
   it 'interpolates between two points' do
     store.save(
       measurement: 'SENEC',
-      field: 'power',
+      field: 'inverter_power',
       timestamp: 1000,
       value: 100,
     )
     store.save(
       measurement: 'SENEC',
-      field: 'power',
+      field: 'inverter_power',
       timestamp: 2000,
       value: 200,
     )
+
     expect(
-      store.interpolate(measurement: 'SENEC', field: 'power', target_ts: 1500),
+      store.interpolate(
+        measurement: 'SENEC',
+        field: 'inverter_power',
+        target_ts: 1500,
+      ),
     ).to eq(150.0)
   end
 
   it 'returns exact value if timestamp matches' do
     store.save(
       measurement: 'SENEC',
-      field: 'power',
+      field: 'inverter_power',
       timestamp: 1000,
       value: 100,
     )
     expect(
-      store.interpolate(measurement: 'SENEC', field: 'power', target_ts: 1000),
+      store.interpolate(
+        measurement: 'SENEC',
+        field: 'inverter_power',
+        target_ts: 1000,
+      ),
     ).to eq(100)
   end
 
   it 'cleans up old data' do
     store.save(
       measurement: 'SENEC',
-      field: 'power',
+      field: 'inverter_power',
       timestamp: 1000,
       value: 100,
     )
     store.save(
       measurement: 'SENEC',
-      field: 'power',
+      field: 'inverter_power',
       timestamp: 2000,
       value: 200,
     )
     store.cleanup(1500)
+
     expect(DB_TEST[:sensor_data].count).to eq(1)
     expect(DB_TEST[:sensor_data].first[:timestamp]).to eq(2000)
   end
