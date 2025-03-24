30763c4df577b97f8205c6689b9b78031514092a
Georg Ledermann <georg@ledermann.dev>
2025-03-24 08:29:10 +0100

    Update influx_writer.rb

--- lib/influx_writer.rb
+++ lib/influx_writer.rb
@@ -2,13 +2,8 @@ class InfluxWriter
   INFLUX_URL = ENV.fetch('INFLUX_URL', 'http://localhost:8086')
 
   class << self
-    def forward_influx_line(
-      influx_line,
-      influx_token:,
-      bucket:,
-      org:,
-      precision:
-    )
+    # Supports single line (String) or multiple lines (Array of Strings)
+    def write(lines, influx_token:, bucket:, org:, precision:)
       client =
         InfluxDB2::Client.new(
           INFLUX_URL,
@@ -16,12 +11,9 @@ class InfluxWriter
           use_ssl: INFLUX_URL.start_with?('https'),
         )
 
-      client.create_write_api.write(
-        data: influx_line,
-        bucket:,
-        org:,
-        precision:,
-      )
+      payload = lines.is_a?(Array) ? lines.join("\n") : lines
+
+      client.create_write_api.write(data: payload, bucket:, org:, precision:)
     ensure
       client&.close!
     end
