186d8744d86b259c14d6351560494bc9b3476301
Georg Ledermann <georg@ledermann.dev>
2025-03-24 09:45:35 +0100

    Update replay_worker_spec.rb

--- spec/replay_worker_spec.rb
+++ spec/replay_worker_spec.rb
@@ -2,22 +2,32 @@ describe ReplayWorker do
   let(:influx_writer) { class_double(InfluxWriter).as_stubbed_const }
 
   before do
+    target_id =
+      STORE.save_target(
+        influx_token: 'test-token',
+        bucket: 'test-bucket',
+        org: 'test-org',
+        precision: 'ns',
+      )
+
     STORE.save(
       measurement: 'SENEC',
       field: 'inverter_power',
       timestamp: 1_000_000_000,
       value: 100,
+      target_id:,
     )
+
     STORE.save(
       measurement: 'Heatpump',
       field: 'power',
       timestamp: 2_000_000_000,
       value: 200,
+      target_id:,
     )
   end
 
   it 'replays unsynced data to InfluxDB and marks as synced' do
-    # Stub InfluxWriter
     allow(InfluxWriter).to receive(:write).and_return(true)
 
     described_class.new(batch_size: 10).replay!
