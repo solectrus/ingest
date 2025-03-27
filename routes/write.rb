640b376acde5b3ca166272e6eec41b924cdcbde5
Georg Ledermann <georg@ledermann.dev>
2025-03-27 20:50:21 +0100

    Update write.rb

--- routes/write.rb
+++ routes/write.rb
@@ -16,10 +16,13 @@ class WriteRoute < BaseRoute
       Processor.new(influx_token, bucket, org, precision).run(influx_line)
       status 204
     rescue InfluxDB2::InfluxError => e
+      warn "#{e}, #{e.backtrace.join("\n")}"
       halt 202, { error: e.message }.to_json
     rescue InvalidLineProtocolError => e
+      warn "#{e}, #{e.backtrace.join("\n")}"
       halt 400, { error: e.message }.to_json
     rescue StandardError => e
+      warn "#{e}, #{e.backtrace.join("\n")}"
       halt 500, { error: e.message }.to_json
     end
   end
