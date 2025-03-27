090315c68af14ffd532b37cbb154a4f2421ac442
Georg Ledermann <georg@ledermann.dev>
2025-03-27 19:41:59 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
-$stdout.sync = true
+require 'logger'
 
 class App < Sinatra::Base
   set :logging, true
+  set :logger, Logger.new($stdout)
+
+  before { env['rack.logger'] = settings.logger }
 
   use StatsRoute
   use WriteRoute
