dc6d743a7b3dfa6d81ea629419dd6ff69bdbace6
Georg Ledermann <georg@ledermann.dev>
2025-03-27 10:02:32 +0100

    Performance

--- db/setup.rb
+++ db/setup.rb
@@ -4,5 +4,10 @@ class DatabaseSetup
       adapter: 'sqlite3',
       database: DBConfig.file,
     )
+
+    ActiveRecord::Base.connection.execute('PRAGMA journal_mode = WAL')
+    ActiveRecord::Base.connection.execute('PRAGMA synchronous = NORMAL')
+
+    ActiveRecord::Base.connection.execute('VACUUM')
   end
 end
