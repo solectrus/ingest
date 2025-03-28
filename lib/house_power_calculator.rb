8d6df7385b3cbd23f343eb6d0e49fea3abaa1bb9
Georg Ledermann <georg@ledermann.dev>
2025-03-28 12:03:22 +0100

    Interpolate via SQL

--- lib/house_power_calculator.rb
+++ lib/house_power_calculator.rb
@@ -6,38 +6,32 @@ class HousePowerCalculator
   attr_reader :target
 
   def recalculate(timestamp:)
-    powers = {}
+    sensor_keys = SensorEnvConfig.sensor_keys_for_house_power
 
-    SensorEnvConfig.sensor_keys_for_house_power.each do |key|
-      sensor = SensorEnvConfig.send(key)
-      next unless sensor
-
-      interpolated =
-        Incoming.interpolate(
-          measurement: sensor[:measurement],
-          field: sensor[:field],
-          timestamp:,
-        )
-
-      powers[key] = interpolated if interpolated
-    end
+    powers =
+      Interpolator.new(
+        timestamp: target.timestamp_ns(timestamp),
+        sensor_keys:,
+      ).run
 
     house_power = HousePowerFormula.calculate(**powers)
     return unless house_power
 
-    line = build_line(house_power, timestamp)
-    target.outgoings.create!(line_protocol: line)
+    write_house_power(house_power, timestamp)
   end
 
   private
 
-  def build_line(house_power, timestamp)
-    Line.new(
-      measurement: SensorEnvConfig.house_power[:measurement],
-      fields: {
-        SensorEnvConfig.house_power[:field] => house_power.round,
-      },
-      timestamp:,
-    ).to_s
+  def write_house_power(house_power, timestamp)
+    line =
+      Line.new(
+        measurement: SensorEnvConfig.house_power[:measurement],
+        fields: {
+          SensorEnvConfig.house_power[:field] => house_power.round,
+        },
+        timestamp:,
+      ).to_s
+
+    target.outgoings.create!(line_protocol: line)
   end
 end
